faaaf43e36891716750413d8ed4bb1fedebc084b
Levi-Ten <porango100@gmail.com>
2021-12-22 16:21:07 +0200

    edit meta

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,9 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta name="description" content="Вдохновляйся работами бьюти-мастеров, общайся на любую тему красоты, узнавай первой о новых тендециях в красоте, новинках косметических производителей ">
+    <meta name="keywords" content="Салон красоты Москва,салон красоты Москва,шугаринг,маникюр,мужской,женский,педикюр,Чистка лица,наращивание ногтей,лифтинг,пилинг,антицеллюлитный массаж.">
+    <meta property="og:url" content="https://deamondparadise.ru">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="beauty salon - сайт для женщин: красота, здоровье, fashion, makeup">
+    <meta property="og:description" content="<p>Вдохновляйся работами бьюти-мастеров, общайся на любую тему красоты, узнавай первой о новых тендециях в красоте, новинках косметических производителей</p>">
+    <title>Салон красоты в центре Москвы | makeup, чистка лица, парикмахерская.</title>
+    <link rel="icon" type="image/x-icon" href="public/img/icons.png">
     <link rel="stylesheet" href="{{ asset("public/css/app.css") }}">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+   
 </head>
 <body>
     @include('templates.main-sidebar')
